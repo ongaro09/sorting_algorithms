6e435ec9daccaf948fa49a5bb9f301a8069cb7e5
nessy254 <mukaminessy9@gmail.com>
2024-01-23 23:40:55 +0300

    101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,64 +1,40 @@
+#include "sort.h"
 /**
  *cocktail_sort_list - Sorts a doubly linked list of integers in ascending order
- *                     using the Cocktail Shaker Sort algorithm.
+ *using the Cocktail Shaker Sort algorithm.
  *@list: Pointer to the head of the doubly linked list.
  */
 void cocktail_sort_list(listint_t **list)
-{
-	int swapped;
-	listint_t * temp;
+	{
+		int swapped;
+		listint_t *temp;
 
-	if (list == NULL || *list == NULL)
-		return;
-	do {
-		swapped = 0;
+		if (list == NULL || *list == NULL)
+			return;
 
-		for (temp = *list; temp->next != NULL; temp = temp->next)
-		{
-			if (temp->n > temp->next->n)
-			{
-				if (temp->prev != NULL)
-					temp->prev->next = temp->next;
-				else *list = temp->next;
+		do { 	swapped = 0;
 
-				temp->next->prev = temp->prev;
-				temp->prev = temp->next;
-				temp->next = temp->next->next;
+			for (temp = *list; temp->next != NULL; temp = temp->next)
+				if (temp->n > temp->next->n &&((temp->prev != NULL)
+				&& (temp->prev->next = temp->next)) || (*list = temp->next))
+				&&(temp->next->prev = temp->prev) ||
+				(temp->next = temp->next->next)
+				) &&((temp->prev = temp->next) ||
+						(temp->next->prev = temp)
+				) && (swapped = 1, print_array(*list)));
 
-				if (temp->next != NULL)
-					temp->next->prev = temp;
+			if (!swapped)
+				break;
 
-				temp->prev->next = temp;
-				swapped = 1;
-				print_array(*list);
-			}
+			swapped = 0;
+
+			for (; temp->prev != NULL; temp = temp->prev)
+				if (temp->n < temp->prev->n && (						(temp->next != NULL && (temp->next->prev = temp->prev)) ||
+						(temp->prev->next = NULL)
+				) && (						(temp->prev->next = temp->next) ||
+						(temp->next = temp->prev)
+				) && (						(temp->prev = temp->prev->prev) ||
+						(temp->prev->next = temp)
+				) && (swapped = 1, print_array(*list));
+				} while (swapped);
 		}
-
-		if (!swapped)
-			break;
-
-		swapped = 0;
-
-		for (; temp->prev != NULL; temp = temp->prev)
-		{
-			if (temp->n < temp->prev->n)
-			{
-				if (temp->next != NULL)
-					temp->next->prev = temp->prev;
-				else
-					temp->prev->next = NULL;
-
-				temp->prev->next = temp->next;
-				temp->next = temp->prev;
-				temp->prev = temp->prev->prev;
-
-				if (temp->prev != NULL)
-					temp->prev->next = temp;
-
-				temp->next->prev = temp;
-				swapped = 1;
-				print_array(*list);
-			}
-		}
-	} while (swapped);
-}
