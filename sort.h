91be4066f248f4902a2a6ae2e3f7203b65a4a928
ongaro09 <ongaromark7@gmail.com>
2024-01-23 16:12:52 +0000

    function prototypes

--- sort.h
+++ sort.h
@@ -26,8 +26,7 @@ void print_array(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
-
+void swap_nodes(listint_t **list, listint_t *left, listint_t *right);
 
 #endif
 
